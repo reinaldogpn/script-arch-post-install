232512fb4b76eb2cdb3a6e933634069b1063b7e6
Reinaldo G. P. Neto <106502559+reinaldogpn@users.noreply.github.com>
2022-06-15 18:02:59 -0300

    Update arch-post-config.sh

--- arch-post-config.sh
+++ arch-post-config.sh
@@ -15,7 +15,6 @@ hostnamectl set-hostname arch
 echo -e arch >> /etc/hostname
 echo -e "127.0.0.1 localhost.localdomain localhost \n::1 localhost.localdomain localhost \n127.0.0.1 arch.localdomain arch" >> /etc/hosts
 echo -e "${VERDE}[INFO] - Configurações de região, data/hora e rede aplicadas.${SEM_COR}"
-exit
 echo -e "${AMARELO}[INFO] - Iniciando instalação de programas... ${SEM_COR}"
 chmod +x arch-post-install.sh
 ./arch-post-install.sh
