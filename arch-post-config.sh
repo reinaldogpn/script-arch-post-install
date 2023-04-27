2372bcd1df2d8c5748005c229a7307b8aa1fafef
Reinaldo G. P. Neto <reinaldo.neto@ufv.br>
2023-04-27 16:46:10 -0300

    Update arch-post-config.sh

--- arch-post-config.sh
+++ arch-post-config.sh
@@ -5,13 +5,30 @@ VERMELHO='\e[1;91m'
 VERDE='\e[1;92m'
 AMARELO='\e[1;93m'
 SEM_COR='\e[0m'
+#
+# echo -e "${VERMELHO} *** Este script deve ser executado no modo super usuário (su -) ***${SEM_COR}"
 
-echo -e "${VERMELHO} *** Este script deve ser executado no modo super usuário (su -) ***${SEM_COR}"
-echo -e "${AMARELO}[INFO] - Aplicando configurações de sistema...${SEM_COR}"
-ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
-hwclock --systohc
-date
-hostnamectl set-hostname arch
-echo -e arch >> /etc/hostname
-echo -e "127.0.0.1 localhost.localdomain localhost \n::1 localhost.localdomain localhost \n127.0.0.1 arch.localdomain arch" >> /etc/hosts
-echo -e "${VERDE}[INFO] - Configurações de região, data/hora e rede aplicadas.${SEM_COR}"
+is_root()
+{
+  # é root?
+  [[ "\$UID" -ne "0" ]] && {  
+    printf '%b' "Execute como r00t.\n"
+    exit 1
+  }
+}
+
+configurar()
+{
+  echo -e "${AMARELO}[INFO] - Aplicando configurações de sistema...${SEM_COR}"
+  ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+  hwclock --systohc
+  date
+  hostnamectl set-hostname arch
+  echo -e arch >> /etc/hostname
+  echo -e "127.0.0.1 localhost.localdomain localhost \n::1 localhost.localdomain localhost \n127.0.0.1 arch.localdomain arch" >> /etc/hosts
+  echo -e "${VERDE}[INFO] - Configurações de região, data/hora e rede aplicadas.${SEM_COR}"
+}
+
+# -------------------------------------
+is_root
+configurar
